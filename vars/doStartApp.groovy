4de8353544eb7b6dca4c59b7f42cfff3e9fe0782
Luis <lfernapalacio@gmail.com>
2018-07-26 11:11:18 +0200

    ci

--- vars/doStartApp.groovy
+++ vars/doStartApp.groovy
@@ -11,21 +11,12 @@ def call(Map<String, Object> config) {
 }
 
 def getConfig(Map<String, Object> config) {
-    //Add build parameters to configFile
-    config.put("groovyVersion", "groovy-2.4.10")
     config.put("git_ref", git_ref)
     config.put("git_url", git_url)
-    //config.put("git_url_ssh", git_url_ssh)
-    //config.put("git_origin", git_origin.contains('tags') ? "tags" : "branch")
-    //config.put("build_number", env.BUILD_ID)
-    //config.put("environment", environment)
 
     //Overrride app in case set in var environment
     if (app != "jenkinsfile") {
         config.app = app
-    }
-    if (!config.boteqa) {
-        config.put("boteqa", false)
     }
     return config
 }
