00ab10cd342a33e72ffc990385330a14d862872c
Luis <luis.fernandez-palacio@capgemini.com>
2020-02-07 10:23:31 +0100

    jmeter

--- vars/doBuildArtifact.groovy
+++ vars/doBuildArtifact.groovy
@@ -7,7 +7,8 @@ def call(Map<String, String> config) {
     node {
         stage('Build') {
             try {
-                maven(util)
+			perfReport filterRegex: '', relativeFailedThresholdNegative: 1.2, relativeFailedThresholdPositive: 1.89, relativeUnstableThresholdNegative: 1.8, relativeUnstableThresholdPositive: 1.5, sourceDataFiles: 'D:\Test_jenkins.jtl'
+               // maven(util)
             } catch (Exception e) {
                 echo '*******************************************'
                 echo e.getMessage()
