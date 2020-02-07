7ef5bd1ef54bb9f3d043091265c9a91e44d506b1
Luis <luis.fernandez-palacio@capgemini.com>
2020-02-07 10:26:27 +0100

    bat

--- vars/doBuildArtifact.groovy
+++ vars/doBuildArtifact.groovy
@@ -7,7 +7,9 @@ def call(Map<String, String> config) {
     node {
         stage('Build') {
             try {
-			perfReport filterRegex: '', relativeFailedThresholdNegative: 1.2, relativeFailedThresholdPositive: 1.89, relativeUnstableThresholdNegative: 1.8, relativeUnstableThresholdPositive: 1.5, sourceDataFiles: 'D:\Test_jenkins.jtl'
+			bat 'D:\Carrefour\SCA-SFC\jmeter_SFC\bin\jmeter.bat -Jjmeter.save.saveservice.output_format=xml -n -t D:\Carrefour\SCA-SFC\jmeter_SFC\backups\SCA-000001.jmx -l D:\Test_jenkins.jtl'
+			
+			//perfReport filterRegex: '', relativeFailedThresholdNegative: 1.2, relativeFailedThresholdPositive: 1.89, relativeUnstableThresholdNegative: 1.8, relativeUnstableThresholdPositive: 1.5, sourceDataFiles: 'D:\Test_jenkins.jtl'
                // maven(util)
             } catch (Exception e) {
                 echo '*******************************************'
